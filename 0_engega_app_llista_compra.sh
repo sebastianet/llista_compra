a0c3b9661ae15c4d4628cb185241616fb926b48d
sebas <sebastiasebas@gmail.com>
2017-02-25 12:46:50 +0100

    sag - package i comande git

---
+++ 0_engega_app_llista_compra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mydate=`date +"y%Y/m%m/d%d"`
+mytime=`date +"h%H:m%M"`
+
+logFN=/home/pi/logs/llisco.log
+szTxt="("$mydate"-"$mytime") +++ +++ +++ LLISCO starts, logging to ("$logFN")."
+logger  -i   -p user.info  $szTxt
+
+sudo  /usr/bin/node  /home/pi/llisco/1_llisco.js    >>  $logFN   2>&1   &

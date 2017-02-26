ca555658511ad2f16f8805891c612a9f3c91d3c6
sebas <sebastiasebas@gmail.com>
2017-02-27 00:41:37 +0100

    sag - CSS sota STATICS, missatge inicial

--- 1_llisco.js
+++ 1_llisco.js
@@ -7,18 +7,19 @@
 // per engegar el servidor   :     0_engega_app_llista_compra.sh
 // per accedir des un client :     http://192.168.1.123:3535       - des la xarxa de casa meva
 //                                 http://IP-EXTERNA:9035          - des el exterior del meu router
-//    com saber la meva IP externa ?
+//    com saber la meva IP externa ?  https://www.whatismyip.com/ - avui (20170227-00:34) es 81.36.193.172
 
 // Versions ( displayed via 'myVersio')
 //     1.1.a - inici del codi amb express
 //     1.1.b - set static files directory
+//     1.1.c - set HOSTNAME and write initial message
 
 
 "use strict";
 
 
-// Moduls que ens calen :
-// ======================
+// moduls que ens calen
+// ====================
 
 var express      = require( 'express' ) ;
 var app = express() ;
@@ -28,10 +29,11 @@ var app = express() ;
      var szOut ;
 
 
-// configuracio :
-// ==============
+// configuracio
+// ============
 
-     app.set( 'mPort', process.env.PORT || 3535 ) ;      // save port to use in APP var ; shall use 3535
+     app.set( 'mPort', process.env.PORT || 3535 ) ;           // save port to use in APP var ; shall use 3535
+     app.set( 'appHostname', require('os').hostname() ) ;
 
 // tell Express to load static files (if there any) from public folder
 
@@ -39,7 +41,42 @@ var app = express() ;
 //     app.use( express.static( path.join(__dirname, 'statics' ) ) ) ;  
      app.use( '/', express.static( __dirname + '/statics' ) ) ;    
 
-// definim les branques a executar segon els que rebem del browser client
+
+// definim algunes funcions
+// ========================
+
+Date.prototype.yyyymmdd = function ( ) {                            // nova funcio yyymmdd de Date() - at server
+
+     var yyyy = this.getFullYear().toString();                                    
+     var mm   = (this.getMonth()+1).toString(); // getMonth() is zero-based         
+     var dd   = this.getDate().toString();
+     return yyyy + '/' + (mm[1]?mm:'0'+mm[0]) + '/' + (dd[1]?dd:'0'+dd[0]);
+
+}; // yyyymmdd()
+
+Date.prototype.hhmmss = function () {
+
+     function fixTime(i) {
+          return (i < 10) ? "0" + i : i;
+     }
+     var today = new Date(),
+          hh = fixTime( today.getHours() ),
+          mm = fixTime( today.getMinutes() ),
+          ss = fixTime( today.getSeconds() ) ;
+     var myHHMMSS = hh + ':' + mm + ':' + ss ;
+     return myHHMMSS ;
+} ; // hhmmss
+
+
+// escriure un missatge inicial a la consola
+// =========================================
+
+    console.log( '+++ +++ +++ +++ +++ +++ +++ +++ app LLISCO starts. Versio [%s], HN [%s], TimeStamp [%s-%s].',
+        myVersio, app.get( 'appHostname' ), (new Date).yyyymmdd(), (new Date).hhmmss() ) ;
+
+
+// definim les branques a executar segons els que rebem del browser client
+// =======================================================================
 
 app.get("/enric", function (req, res){
      szOut = 'Hola. ' + myVersio ;
@@ -52,9 +89,8 @@ app.get("/",function (req, res){
 });
 
 
-
-// creacio del servidor :
-// ======================
+// creacio del servidor
+// ====================
 
 var server = app.listen( app.get( 'mPort' ), '192.168.1.123', function () {
 // var server = app.listen( app.get( 'mPort' ), '127.0.0.1', function () {
